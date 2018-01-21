3fc5e320984be352c6ec2b7de5f5b8272c00545f
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-22 00:36:42 +0530

    Apple Scripts

--- pakg/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-with open('/Users/pyadhe/Documents/iTest/testBash/macBash/test.txt', 'a') as the_file:
-    for x in sys.argv:
-        the_file.write(x+"\t")
-    the_file.write("\n")
